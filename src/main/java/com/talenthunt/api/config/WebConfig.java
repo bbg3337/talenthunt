9cda8cf9a71b72b47e7f8e97d8821ac952dc97ee
Haramohan Nanda <haramohan.nanda@gmail.com>
2019-02-16 14:26:15 +0530

    Swagger

--- src/main/java/com/talenthunt/api/config/WebConfig.java
+++ src/main/java/com/talenthunt/api/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.talenthunt.api.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,4 +14,5 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+    
 }
